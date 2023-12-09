814bd2008675b13a97b7ed8a40159489b7031023
ks91 <ks91@sfc.wide.ad.jp>
2023-12-09 20:06:54 +0900

    updated the order of demonstration.

--- 散布図と相関.R
+++ 散布図と相関.R
@@ -1,11 +1,12 @@
 # このサンプルは、1コマンドずつ実行してみてください。
 
 表1 <- read.table("英語数学成績.txt", header=T)
-表2 <- read.table("運動成績.txt", header=T)
 
 plot(表1, xlim=c(0, 100), ylim=c(0, 100),
      xlab="英語の試験結果", ylab="数学の試験結果",
      main="正の相関の例")
+
+表2 <- read.table("運動成績.txt", header=T)
 
 plot(表2, xlim=c(0, 20.0), ylim=c(13.0, 18.0),
      xlab="週当たりの課外運動時間",
